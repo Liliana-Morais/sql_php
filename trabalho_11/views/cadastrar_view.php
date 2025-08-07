ebb241da26ad9d2736615a2bc6d67f241cf7adf0
Liliana Morais <lilianamorais1@gmail.com>
2025-08-08 00:05:12 +0100

    Update cadastrar_view.php

--- trabalho_11/views/cadastrar_view.php
+++ trabalho_11/views/cadastrar_view.php
@@ -1,1 +1,18 @@
+<main class="container-fluid my-4">
 
+  <div class="caixa">
+
+    <h2 class="text-uppercase">Cadastrar</h2>
+
+    <form action="">
+      <input type="text" name="nome" placeholder="Nome" required autofocus>
+      <br><br>
+      <input type="number" name="preco" placeholder="Preço" required min="0.01" step="0.01">
+      <br><br>
+      <input type="number" name="quantidade" placeholder="Quantidade" min="0" required>
+      <br><br>
+      <input type="submit" value="Cadastrar">
+    </form>
+  </div>
+
+</main>
